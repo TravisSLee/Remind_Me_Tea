bb7e3cd637d6d498de71cf12e60a28dfd1810a6f
TravisSLee <travisslee223@gmail.com>
2020-10-28 18:52:07 -0400

    Updated user controller and created bbt views

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,5 +4,55 @@ class UserController < ApplicationController
    erb :index
   end
 
+  get '/signup' do
+    if !logged_in?
+      erb :'/users/signup'
+    else
+      redirect to '/tweets'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+     redirect '/signup'
+    else
+     @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    end
+   end
+
+
+  get "/users/:slug" do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
+
+  get '/login' do
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect '/tweets'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/signup'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+      redirect '/login'
+    else
+      redirect '/'
+    end
+  end
 
 end
