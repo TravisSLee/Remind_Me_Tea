3e622d20e71649bc207135c4059583f92a43a715
TravisSLee <travisslee223@gmail.com>
2020-10-31 13:03:41 -0400

    Updating bbts and user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
     @user = User.find_by(:username => params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect '/'
+      redirect '/bbts'
     else
       redirect '/signup'
     end
