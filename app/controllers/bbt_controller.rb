15b37c07a836c7d3d4734d49134ddf3ba700eb8d
TravisSLee <travisslee223@gmail.com>
2020-11-01 14:34:15 -0500

    Implemented working delete route

--- app/controllers/bbt_controller.rb
+++ app/controllers/bbt_controller.rb
@@ -46,7 +46,7 @@ class BbtsController < ApplicationController
 
   get '/bbts/:id/edit' do
     if logged_in?
-      @bbt = Bbt.find(params[:id])
+      @bbt = Bbt.find_by_id(params[:id])
       erb :'bbts/edit'
     else
       redirect "/login"
@@ -55,30 +55,22 @@ class BbtsController < ApplicationController
 
   patch '/bbts/:id' do
     if logged_in?
-      @bbt = Bbt.find_by(params[:id])
-      if !params[:brand] == ""
+      bbt = Bbt.find_by_id(params[:id])
+      if params[:brand] == ""
         redirect to "/bbts/#{@bbt.id}/edit"
       else
-        @bbt.brand = params[:brand]
-        @bbt.sugar_amount = params[:sugar_amount]
-        @bbt.size = params[:size]
-        @bbt.temp = params[:temp]
-        @bbt.save
+        bbt.update(bubble: params[:bubble], brand: params[:brand], sugar_amount: params[:sugar_amount], size: params[:size], temp: params[:temp])
       end
     else
       redirect '/login'
     end
   end
 
-  delete '/bbts/:id/delete' do
+  delete '/bbts/:id' do
     if logged_in?
-      @bbt = bbt.find_by_id(params[:id])
-      if @bbt.user_id == current_user.id
-        @btt.delete
-        redirect to '/bbt'
-      else
-        erb :'bbt/error'
-      end
+      @bbt = Bbt.find(params[:id])
+      @bbt.destroy
+      redirect to '/bbts'
     else
       redirect to '/login'
     end
