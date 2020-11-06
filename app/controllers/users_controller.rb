543ce2b590016294fa3d4843daf44dff847af5ce
Travis Lee <70967848+TravisSLee@users.noreply.github.com>
2020-11-05 21:13:24 -0500

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,18 @@ class UsersController < ApplicationController
       redirect to '/bbts'
     end
   end
+  
+  post '/signup' do
+    if params[:password] == ""
+     redirect '/signup'
+    else
+     @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+      if @user.save
+        session[:user_id] = @user.id
+        redirect '/bbts'
+      else
+        redirect '/signup'
+      end
+    end
+  end
 end
